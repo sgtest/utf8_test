6e9ec85292e56c28a9da61e973a0939ddd0ea3f7
Quinn Slack <qslack@qslack.com>
2014-06-01 19:53:05 -0700

    utf8 test packages

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='utf8_test',
+      version='1.0',
+      description='utf8 test for Sourcegraphh',
+      author='Quinn Slack',
+      author_email='sqs@sourcegraph.com',
+      packages=['utf8'],
+)
